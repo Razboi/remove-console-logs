759c7e2926b405234f3051a0b590112b777f9da5
Marc <recatala.marc@gmail.com>
2021-05-15 16:09:02 +0200

    Added logic to handle directories. Refactored code to be more modular

---
+++ lib/helpers/list-directory-files.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (directoryPath: string) => Promise<string[]>;
+export default _default;
---
+++ lib/helpers/sanitize-file.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (filePath: string) => Promise<void>;
+export default _default;
